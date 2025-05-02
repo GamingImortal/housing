f4fd66f17e637bf3381404844ed73c5902928874
GamingImortal <chiwesheralph@gmail.com>
2025-05-02 20:57:58 +0200

    47:09 frontend!

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./HomePage.scss";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 export default function HomePage() {
   return (
@@ -13,6 +14,19 @@ export default function HomePage() {
           Check out and book our various listed houses with suitable living
           conditions for students
         </p>
+        <SearchBar />
+        <div className="box">
+          <h1></h1>
+          <h2></h2>
+        </div>{" "}
+        <div className="box">
+          <h1></h1>
+          <h2></h2>
+        </div>{" "}
+        <div className="box">
+          <h1></h1>
+          <h2></h2>
+        </div>
       </div>
       <div className="imageContainer">
         <img src="https://www.campusperth.com/assets/img/campus-rooms-2.gif?v1"></img>
