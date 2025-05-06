ea1b3c151a8c0d9dd7b0abe3193150526fb8f6e1
GamingImortal <chiwesheralph@gmail.com>
2025-05-06 21:28:30 +0200

    2hrs 30min

--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SinglePage.scss";
 import Slider from "../../components/slider/Slider";
 import LocationPinIcon from "@mui/icons-material/LocationPin";
+import "./SinglePage.scss";
+import Map from "../../components/map/Map";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import GroupsIcon from "@mui/icons-material/Groups";
+import WavesIcon from "@mui/icons-material/Waves";
+import BoltIcon from "@mui/icons-material/Bolt";
+import MyLocationIcon from "@mui/icons-material/MyLocation";
+import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
+import WcIcon from "@mui/icons-material/Wc";
+import NetworkWifiIcon from "@mui/icons-material/NetworkWifi";
+import MicrowaveIcon from "@mui/icons-material/Microwave";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from "@mui/material";
 
 export const SinglePage = () => {
+  const [save, setSave] = useState(false);
+  const [submit, setSubmit] = useState(false);
+
   const singledata = {
+    id: 1,
+    title: "Luxury Villa",
+    // ...
+    latitude: -17.7695,
+    longitude: 31.041,
     id: 1,
     title: "Luxury Villa",
     price: "$500,000",
@@ -53,7 +75,73 @@ export const SinglePage = () => {
         </div>
       </div>
       <div className="features">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <p className="general">General</p>
+          <div className="listVertical">
+            <p className="title">
+              <MoreHorizIcon />
+              Status:{" "}
+            </p>
+            <p className="title">
+              <MyLocationIcon />
+              Distance From Campus:{" "}
+            </p>
+            <p className="title">
+              <WcIcon />
+              Gender:{" "}
+            </p>
+            <p className="title">
+              <GroupsIcon />
+              People in a Room:{" "}
+            </p>
+          </div>
+
+          <p className="service">Services</p>
+          <div className="services">
+            <p className="title">
+              <BoltIcon />
+              Solar BackUp:{" "}
+            </p>
+            <p className="title">
+              <WavesIcon />
+              Water:{" "}
+            </p>
+            <p className="title">
+              <NetworkWifiIcon />
+              Unlimited Wifi:{" "}
+            </p>
+            <p className="title">
+              <OutdoorGrillIcon />
+              Stove:{" "}
+            </p>
+            <p className="title">
+              <MicrowaveIcon />
+              Stove BackUp:{" "}
+            </p>
+            <p className="title">
+              <MyLocationIcon />
+              Address:{" "}
+            </p>
+          </div>
+          <p className="location">Location on Map</p>
+          <div className="mapContainer">
+            <Map items={[singledata]} />
+          </div>
+          <div className="buttons">
+            {save === false ? (
+              <BookmarkBorderOutlinedIcon onClick={() => setSave(!save)} />
+            ) : (
+              <BookmarkIcon onClick={() => setSave(!save)} />
+            )}
+            {submit === false ? (
+              <Button className="outlined" onClick={() => setSubmit(!submit)}>
+                Book
+              </Button>
+            ) : (
+              <Button type="disabled">Submitted</Button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
